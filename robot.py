c3ec46b559e0cb3b79ba1f887f04dbadb043081d
Rod Hinman <rod@auroraresearch.com>
2020-01-25 10:54:15 -1000

    Color reader test (successful)

--- robot.py
+++ robot.py
@@ -4,8 +4,9 @@
 """
 
 import wpilib
-import ctre
-import wpilib.drive
+#import ctre
+#import wpilib.drive
+from rev.color import ColorSensorV3
 
 
 class MyRobot(wpilib.TimedRobot):
@@ -15,29 +16,39 @@ class MyRobot(wpilib.TimedRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
+        self.colorSensor = ColorSensorV3(wpilib.I2C.Port.kOnboard)
+
+        self.temp = 1
 
 
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
-
-
-
+        pass
 
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
-
-
-
+        pass
 
 
     def teleopInit(self):
-
+        pass
 
 
     def teleopPeriodic(self):
 
+        color = self.colorSensor.getColor()
+        ir = self.colorSensor.getIR()
+
+        red = color.red
+        blue = color.blue
+        green = color.green
+
+        self.temp += 1
+        if self.temp % 49 == 0:
+            print(self.temp, color.red, color.green, color.blue)
 
 
 if __name__ == "__main__":
-    wpilib.run(MyRobot)f.r_joy = wpilib.Joystick(1)
+    wpilib.run(MyRobot)
+
