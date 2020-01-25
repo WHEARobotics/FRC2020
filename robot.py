ac72bd8fd454af9f90196f7e01d03ac41e2ea965
Rod Hinman <rod@auroraresearch.com>
2020-01-25 13:49:45 -1000

    We finished color-Matching software for Red Green Blue and Yellow

--- robot.py
+++ robot.py
@@ -7,6 +7,7 @@ import wpilib
 #import ctre
 #import wpilib.drive
 from rev.color import ColorSensorV3
+from rev.color import ColorMatch
 
 
 class MyRobot(wpilib.TimedRobot):
@@ -16,7 +17,25 @@ class MyRobot(wpilib.TimedRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
+
+        #This configures the color sensor
         self.colorSensor = ColorSensorV3(wpilib.I2C.Port.kOnboard)
+        #This defines the color matching prosses
+        self.colormatcher = ColorMatch()
+        #This defines the how confident in the chosen color the matcher must be
+        self.colormatcher.setConfidenceThreshold(0.95)
+
+        #These define each color by its RGB values
+        self.BlueTarget = wpilib.Color(0.143, 0.427, 0.429)
+        self.GreenTarget = wpilib.Color(0.197, 0.561, 0.240)
+        self.RedTarget = wpilib.Color(0.561, 0.232, 0.114)
+        self.YellowTarget = wpilib.Color(0.361, 0.524, 0.113)
+
+        #This adds our target values to colormatcher
+        self.colormatcher.addColorMatch(self.BlueTarget)
+        self.colormatcher.addColorMatch(self.GreenTarget)
+        self.colormatcher.addColorMatch(self.RedTarget)
+        self.colormatcher.addColorMatch(self.YellowTarget)
 
         self.temp = 1
 
@@ -36,17 +55,37 @@ class MyRobot(wpilib.TimedRobot):
 
 
     def teleopPeriodic(self):
-
+        #This has the color sensor collect color values
         color = self.colorSensor.getColor()
-        ir = self.colorSensor.getIR()
+        #defines colorstring
+        colorstring = 'Unknown'
+        #resets confidence
+        confidence = 0.95
+        #uses confidence factor to determine closest color values
+        matchedcolor = self.colormatcher.matchClosestColor(color, confidence)
 
+        #uses estimated color values to return exact preset colors for printing
+        if matchedcolor.red == self.BlueTarget.red and matchedcolor.green == self.BlueTarget.green and matchedcolor.blue == self.BlueTarget.blue:
+            colorstring = 'blue'
+
+        elif matchedcolor.red == self.RedTarget.red and matchedcolor.green == self.RedTarget.green and matchedcolor.blue == self.RedTarget.blue:
+            colorstring = 'red'
+
+        elif matchedcolor.red == self.GreenTarget.red and matchedcolor.green == self.GreenTarget.green and matchedcolor.blue == self.GreenTarget.blue:
+            colorstring = 'green'
+        elif matchedcolor.red == self.YellowTarget.red and matchedcolor.green == self.YellowTarget.green and matchedcolor.blue == self.YellowTarget.blue:
+            colorstring = 'yellow'
+
+        #defines color values
         red = color.red
         blue = color.blue
         green = color.green
 
+        #keeps pace and prints results
         self.temp += 1
         if self.temp % 49 == 0:
-            print(self.temp, color.red, color.green, color.blue)
+            print ('{:5.3f} {:5.3f} {:5.3f} {} {} {:5.3f} {:5.3f} {:5.3f}'.format(color.red, color.green, color.blue, colorstring, confidence, matchedcolor.red, matchedcolor.green, matchedcolor.blue))
+
 
 
 if __name__ == "__main__":
